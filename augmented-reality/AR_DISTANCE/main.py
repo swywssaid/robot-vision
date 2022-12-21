a673ac739890bf36ecfa016d304ebf4edffd585e
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-12-22 03:13:22 +0900

    feat: Create videoWrite

--- augmented-reality/AR_DISTANCE/main.py
+++ augmented-reality/AR_DISTANCE/main.py
@@ -19,6 +19,11 @@ marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 param_markers = aruco.DetectorParameters_create()
 
 cap = cv2.VideoCapture("./augmented-reality/AR_BASIC/images/marker.mp4")
+w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
 
 while True:
     ret, frame = cap.read()
@@ -77,9 +82,11 @@ while True:
             
     frame = cv2.flip(frame, 1)
     frame = cv2.flip(frame, 0)
+    out.write(frame)
     cv2.imshow("frame", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
